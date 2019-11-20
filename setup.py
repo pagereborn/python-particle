9781c6dbe23aaf8d0b812a1981ee6494871ff9d4
Steven Yang <queencarservice@gmail.com>
2019-11-20 17:44:33 -0500

    updated install_requires

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ setup(
     description='A Python wrapper around the Particle (particle.io) Cloud API',
     long_description=README,
     install_requires = [
-        'python-dateutil==2.4.2',
-        'pytz==2015.4',
+        'python-dateutil==2.8.1',
+        'pytz==2019.3',
         'requests==2.7.0',
 	'pexpect==3.3'
     ],
